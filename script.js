3348c5abdde05f2f848b5234f588c126c7c02406
shuaibu abdulkadir hussain <114912137+shuaibu222@users.noreply.github.com>
2022-12-22 00:42:17 +0100

    update
    update and deletion of design images

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const hamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const sidebar = document.querySelector("#mySidenav");
 const closeSidebar = document.querySelector("#closeSidebar");
+const overlay = document.querySelector("#overlayId");
 
 hamburger.addEventListener("click", function () {
   sidebar.style.width = "270px";
@@ -13,4 +14,5 @@ hamburger.addEventListener("click", function () {
 closeSidebar.addEventListener("click", function () {
   sidebar.style.width = "0px";
   body.style.backgroundColor = "hsl(36, 100%, 99%)";
+  body.style.overflow = "visible";
 });
